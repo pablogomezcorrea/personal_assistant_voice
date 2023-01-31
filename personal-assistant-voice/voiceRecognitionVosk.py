1445a1ece82da125f533242306178ac29f9478d5
Pablo Correa <pablocorrea@MacBook-Pro-de-Pablo.local>
2023-01-31 00:42:46 -0300

    Adding vosk voice recognition

---
+++ personal-assistant-voice/voiceRecognitionVosk.py
@@ -0,0 +1,24 @@
+# from pyaudio._portaudio import start_stream
+from vosk import Model, KaldiRecognizer
+import pyaudio
+import os
+
+model = Model("../model")
+rec = KaldiRecognizer(model, 16000)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+
+while True:
+    data = stream.read(10000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
+
+print(rec.FinalResult())
+
+
